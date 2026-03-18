f4c0143cf6c18a2ed068b85ae8396b4ee0af5ce9
yn3104ao <aoi-yano@oighear.art>
2026-03-19 02:44:34 +0900

    fix: some mistakes

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -4,7 +4,6 @@ local wezterm = require("wezterm")
 -- Load Plugins
 -- ==========================================
 local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez")
--- tabline.apply_to_config(config)
 tabline.setup()
 
 
